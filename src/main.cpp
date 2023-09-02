ae278aeab60826e22df6f1edd88382fdeaf895cb
behova <szulauf90@gmail.com>
2023-09-02 14:55:50 -0700

    rejiged the game loop

--- src/main.cpp
+++ src/main.cpp
@@ -25,16 +25,108 @@ private:
   int currentRotation;
   int currentX;
   int currentY;
-  int pieceLocked;
+  int pieceLock;
   double delayTime;
   double fAccumulatedTime;
   int pause;
   int input;
 
 public:
-  bool OnUserCreate() override {
-    // Called once at the start, so create things here
+  void mainDrawLoop() {
+    if (pieceLock == 0) {
 
+      for (int x = 0; x < ScreenWidth(); x++) {
+        for (int y = 0; y < ScreenHeight(); y++) {
+          Draw(x, y, olc::Pixel(0, 0, 0));
+        }
+      }
+
+      gameBoard.resetBoard();
+
+      gameBoard.addPiece(currentPiece, currentRotation, currentX, currentY);
+
+      UI.drawBoard(this, &gameBoard.currentBoard);
+
+      UI.drawUI(this, score, level, lines, nextPiece);
+    }
+  }
+  void collisionCheck() {
+    if (pieceLock == 0) {
+      pieceLock = gameBoard.checkCollision(currentPiece, currentRotation,
+                                           currentX, currentY);
+    }
+  }
+  void pieceLocked() {
+    if (pieceLock == 1) {
+      gameBoard.addLocked(currentPiece, currentRotation, currentX, currentY);
+
+      // increase score for piece lock(check last key press to see if
+      // hardlock. check current peice for amount scored)
+
+      // reset y x and piecelock
+      currentRotation = 0;
+      currentY = 0;
+      currentX = 3;
+      pieceLock = 0;
+
+      // set the next tetrimino
+      currentPiece = nextPiece;
+      nextPiece = tetriminos.getRandomTetrimino();
+
+      // check for row match
+      lines += gameBoard.checkLines();
+
+      // increase level for line accumulation
+      if (logic.getLevel(level, lines) == true) {
+        level += 1;
+        lines = 0;
+        delayTime = logic.getDelayTime(level);
+      }
+
+      // check for gameOver
+      gameOver = gameBoard.checkGameOver();
+    }
+  }
+  void advanceRow() {
+    if (fAccumulatedTime >= delayTime) {
+
+      if (gameBoard.checkCollision(currentPiece, currentRotation, currentX,
+                                   currentY + 1) == 0) {
+
+        currentY += 1;
+
+      } else {
+        pieceLock = 1;
+      }
+
+      // reset accumulated time
+      fAccumulatedTime = 0;
+
+      // score += 1;
+    }
+  }
+  void pauseLoop() {
+
+    gameBoard.resetBoard();
+
+    gameBoard.addPiece(currentPiece, currentRotation, currentX, currentY);
+
+    UI.drawBoard(this, &gameBoard.currentBoard);
+
+    UI.drawPause(this, score, level, lines);
+
+    if (GetKey(olc::ESCAPE).bPressed) {
+      pause = 0;
+    }
+  }
+  void gameOverLoop() {
+
+    UI.drawBoard(this, &gameBoard.currentBoard);
+
+    UI.drawGameOver(this, score, level, lines);
+  }
+  bool OnUserCreate() override {
+    // Called once at the start
     // set rows and columns
     rows = 26;
     columns = 11;
@@ -48,7 +140,7 @@ public:
     gameOver = 0;
     // score global
     score = 0;
-    level = 8;
+    level = 0;
     lines = 0;
     // info for drawing pieces
     nextPiece = tetriminos.getRandomTetrimino();
@@ -58,7 +150,7 @@ public:
     currentX = 3;
     currentY = 0;
     // when piece is locked it's added to locked array
-    pieceLocked = 0;
+    pieceLock = 0;
     // delay will decrease as time goes on. checked against fElaspedTime
     delayTime = logic.getDelayTime(level);
     // acumulate fElapsed time
@@ -85,175 +177,62 @@ public:
   {
     // calculate time interval
     fAccumulatedTime += fElapsedTime;
-
-    // reset input
-    input = 0;
-
     /*pause game loop*/
+    /*Game over screen*/
+    /*main game loop*/
+
+    /* Handle user input*/
+
     if (pause == 1) {
-      gameBoard.resetBoard();
-
-      gameBoard.addPiece(currentPiece, currentRotation, currentX, currentY);
-
-      UI.drawBoard(this, &gameBoard.currentBoard);
-
-      UI.drawPause(this, score, level, lines);
-
-      if (GetKey(olc::SPACE).bPressed) {
-        pause = 0;
-        for (int x = 0; x < ScreenWidth(); x++) {
-          for (int y = 0; y < ScreenHeight(); y++) {
-            Draw(x, y, olc::Pixel(0, 0, 0));
-          }
+      pauseLoop();
+    } else if (gameOver == 1) {
+      gameOverLoop();
+    } else {
+      if (GetKey(olc::LEFT).bPressed) {
+        if (gameBoard.checkCollision(currentPiece, currentRotation,
+                                     currentX - 1, currentY) == 0) {
+          currentX -= 1;
         }
       }
+      if (GetKey(olc::UP).bPressed) {
+        if (gameBoard.checkCollision(currentPiece, currentRotation + 1,
+                                     currentX, currentY) == 0) {
+          currentRotation += 1;
+        }
+      }
+      if (GetKey(olc::RIGHT).bPressed) {
+        if (gameBoard.checkCollision(currentPiece, currentRotation,
+                                     currentX + 1, currentY) == 0) {
+          currentX += 1;
+        }
+      }
+      if (GetKey(olc::DOWN).bPressed) {
+        if (gameBoard.checkCollision(currentPiece, currentRotation, currentX,
+                                     currentY + 1) == 0) {
+          currentY += 1;
+        }
 
-    }
-    /*Game over screen*/
-    else if (gameOver == 1) {
-      UI.drawBoard(this, &gameBoard.currentBoard);
-
-      UI.drawGameOver(this, score, level, lines);
-
-    }
-    /*main game loop*/
-    else {
-
-      /* Handle user input*/
+        // change advancing of row here
+      }
+      if (GetKey(olc::F1).bPressed) {
+        level += 1;
+        delayTime = logic.getDelayTime(level);
+      }
       if (GetKey(olc::ESCAPE).bPressed) {
         pause = 1;
       }
-      if (GetKey(olc::LEFT).bPressed) {
-        input = 1;
-      }
-      if (GetKey(olc::UP).bPressed) {
-        input = 2;
-      }
-      if (GetKey(olc::RIGHT).bPressed) {
-        input = 3;
-      }
-      if (GetKey(olc::DOWN).bPressed) {
-        input = 4;
-      }
-      if (GetKey(olc::F1).bPressed) {
-        input = 5;
-      }
-      // only one move can be made per frame
-      if (pieceLocked == 0) {
-        switch (input) {
-        case 1:
-          if (gameBoard.checkCollision(currentPiece, currentRotation,
-                                       currentX - 1, currentY) == 0) {
-            currentX -= 1;
-            break;
-          } else {
-            break;
-          }
-        case 2:
-          if (gameBoard.checkCollision(currentPiece, currentRotation + 1,
-                                       currentX, currentY) == 0) {
-            currentRotation += 1;
-            break;
-          } else {
-            break;
-          }
-        case 3:
-          if (gameBoard.checkCollision(currentPiece, currentRotation,
-                                       currentX + 1, currentY) == 0) {
-            currentX += 1;
-            break;
-          } else {
-            break;
-          }
-        case 4:
-          if (gameBoard.checkCollision(currentPiece, currentRotation, currentX,
-                                       currentY + 1) == 0) {
-            currentY += 1;
-            break;
-          } else {
-            break;
-          }
-        case 5:
-          level += 1;
-          delayTime = logic.getDelayTime(level);
-          break;
-        default:
-          break;
-        }
-      }
-
       /* Catch all collision check*/
-      if (pieceLocked == 0) {
-        pieceLocked = gameBoard.checkCollision(currentPiece, currentRotation,
-                                               currentX, currentY);
-      }
+      collisionCheck();
 
-      /*If piece is locked reset tetrimino variables and switch to next piece*/
-      if (pieceLocked == 1) {
-        gameBoard.addLocked(currentPiece, currentRotation, currentX, currentY);
-
-        // increase score for piece lock(check last key press to see if
-        // hardlock. check current peice for amount scored)
-
-        // reset y x and piecelock
-        currentRotation = 0;
-        currentY = 0;
-        currentX = 3;
-        pieceLocked = 0;
-
-        // set the next tetrimino
-        currentPiece = nextPiece;
-        nextPiece = tetriminos.getRandomTetrimino();
-
-        // check for row match
-        lines += gameBoard.checkLines();
-
-        // increase level for line accumulation
-        if (logic.getLevel(level, lines) == true) {
-          level += 1;
-          lines = 0;
-          delayTime = logic.getDelayTime(level);
-        }
-
-        // check for gameOver
-        gameOver = gameBoard.checkGameOver();
-      }
+      /*If piece is locked reset tetrimino variables and switch to next
+       * piece*/
+      pieceLocked();
 
       /* Main draw loop*/
-      if (pieceLocked == 0) {
-
-        for (int x = 0; x < ScreenWidth(); x++) {
-          for (int y = 0; y < ScreenHeight(); y++) {
-            Draw(x, y, olc::Pixel(0, 0, 0));
-          }
-        }
-
-        gameBoard.resetBoard();
-
-        gameBoard.addPiece(currentPiece, currentRotation, currentX, currentY);
-
-        UI.drawBoard(this, &gameBoard.currentBoard);
-
-        UI.drawUI(this, score, level, lines, nextPiece);
-      }
+      mainDrawLoop();
 
       /*Advance tetrimino on time interval*/
-      if (fAccumulatedTime >= delayTime) {
-
-        if (gameBoard.checkCollision(currentPiece, currentRotation, currentX,
-                                     currentY + 1) == 0) {
-
-          currentY += 1;
-
-        } else {
-          pieceLocked = 1;
-        }
-
-        // reset accumulated time
-        fAccumulatedTime = 0;
-
-        // score += 1;
-      }
+      advanceRow();
     }
 
     return true;
